d79f23f0e424d804a48e76fe029534216f89d23c
Georges A. Coly <ageocoly@gmail.com>
2016-10-04 14:04:47 +0000

    users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < application_controller
+
+
+  def index
+      @users = User.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
