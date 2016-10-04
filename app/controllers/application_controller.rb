837233e1a2b3c460d3f568fb1881a008c6b02d7a
Georges A. Coly <ageocoly@gmail.com>
2016-10-04 20:14:32 +0000

    to micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Hello, word!"
+  def m_post
+    redirect_to microposts_url
   end
 end
